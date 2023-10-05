308c0d06aff6b1d85652c403b13ba7b24ad02ba9
Andi-gech <andi.fab23@gmail.com>
2023-10-05 18:56:03 +0300

    update 2.0

--- src/Newspage.js
+++ src/Newspage.js
@@ -2,19 +2,24 @@ import NewsCard from "./NewsCard";
 import useNews from "./UseNews";
 
 function NewsPage() {
-    const { data } = useNews()
-    
+  const { data } = useNews();
 
-    return ( <div className="Newspage">
-        <div className="NewsTitle">
-            <p>Latest Product News</p>
-        </div>
-        <div className="NewsList">
-            {data?.map((item,index)=> <NewsCard key={index} headline={item.Headline} description={item.description}/>)}
-           
-           
-        </div>
-    </div>);
+  return (
+    <div className="mt-[60px] bg-white   dark:bg-zinc-900 min-h-screen flex items-center flex-col">
+      <div className="NewsTitle">
+        <p>Latest Product News</p>
+      </div>
+      <div className=" w-full sm:w-1/3 h-[200px]">
+        {data?.map((item, index) => (
+          <NewsCard
+            key={index}
+            headline={item.Headline}
+            description={item.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default NewsPage;
